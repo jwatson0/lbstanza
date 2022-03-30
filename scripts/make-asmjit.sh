0614c52f8a448ce92d54e6eee547bc8c8131f680
Patrick Li <patrickli.2001@gmail.com>
2022-03-30 09:33:44 -0700

    Added build scripts for ASMJit.

--- scripts/make-asmjit.sh
+++ scripts/make-asmjit.sh
@@ -1,8 +1,4 @@
-CURRENTDIR=$PWD
-mkdir -p build/asmjit
 cmake -S libs/asmjit -B build/asmjit
-cd build/asmjit
-make
-cd $CURRENTDIR
+cmake --build build/asmjit
 mv build/asmjit/libasmjit.a bin/libasmjit.a
 rm -r build/asmjit
